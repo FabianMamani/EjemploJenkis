c0296b72521ecc8889d11822040345b7c62ee3ce
fabian.mamani <fabian.mamani@AR-NB-071.tsoftglobal.com>
2019-09-23 12:50:01 -0300

    Cambio de implementación.

--- Proyecto/src/main/HolaMundo.java
+++ Proyecto/src/main/HolaMundo.java
@@ -1,12 +1,18 @@
 package main;
 
+import java.util.Scanner;
+
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
-		System.out.println("Desde Eclipse..");
-		System.out.println("Realizando Cambios...");
-		System.out.println("Probando...");
+		Scanner entrada = new Scanner(System.in);
+		double area;
+		double per=1;
+		System.out.println("Ingrese el area del cuadrado: ");
+		area = entrada.nextDouble();
+		per = Math.sqrt(area);
+		per= per*4;
+		System.out.println(per);
 	}
 
 }
